9f887966c3179171de0eec8cd556864d47cacb5a
Frozenk <frozenk@frozenk.fr>
2025-07-02 15:58:53 +0200

    Create install_nxcwraper.sh

---
+++ install_nxcwraper.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+pip install colorama >/dev/null 2>&1
+
+curl -sSL https://raw.githubusercontent.com/Frozenka/nxcwrap/refs/heads/main/nxcwrap.py -o /opt/tools/NetExec/nxc/nxcwrap.py
+chmod +x /opt/tools/NetExec/nxc/nxcwrap.py
+
+mkdir -p /root/.nxc
+
+CONF_PATH="/root/.nxc/nxc.conf"
+if ! grep -q "\[Exegol-History\]" "$CONF_PATH" 2>/dev/null; then
+    echo -e "\n[Exegol-History]\nscrap = True" >> "$CONF_PATH"
+else
+    sed -i '/\[Exegol-History\]/,/^\[/{s/^scrap *=.*/scrap = True/}' "$CONF_PATH"
+fi
+
+ALIAS_LINE='alias nxc="python3 /opt/tools/NetExec/nxc/nxcwrap.py"'
+if ! grep -Fxq "$ALIAS_LINE" /root/.bashrc; then
+    echo "$ALIAS_LINE" >> /root/.bashrc
+fi
